c6a0f0cca889c0aa539409ed4d337af4c3a463d2
Jimmy Vrana <devJimmyboy@gmail.com>
2025-05-01 12:34:13 -0500

    Update home page txt

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -6,7 +6,7 @@ export default function HomePage({}: Props) {
   return (
     <Stack align="center" justify="center">
       <div className="text-4xl font-bold text-shadow-md text-shadow-black">Hvdras Bot</div>
-      <div className="text-2xl font-bold text-shadow-md text-shadow-black">A Discord bot for the Hvdras community</div>
+      <div className="text-2xl font-bold text-shadow-md text-shadow-black">a dumb fucking bot</div>
     </Stack>
   )
 }
