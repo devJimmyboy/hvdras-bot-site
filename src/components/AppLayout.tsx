215fa129b8445eca7c55d57f6f02774d0fd27048
Jimmy Vrana <devJimmyboy@gmail.com>
2025-05-01 12:58:29 -0500

    updates

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -7,9 +7,9 @@ type Props = {}
 export default function AppLayout({}: Props) {
   const location = useLocation()
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-screen">
       <Box className="flex flex-row gap-4  items-center p-4 sticky top-0 z-10 " style={{ backgroundColor: 'var(--mantine-color-dark-6)', height: '5vh', minHeight: '80px' }}>
-        <img src="https://cdn.7tv.app/emote/01G7HBWGZ8000DJJQKZ99T0A2Y/2x.avif" alt="Logo" className="w-12 h-12 rounded-lg drop-shadow-lg drop-shadow-black/60" />
+        <img src="https://cdn.7tv.app/emote/01G7HBWGZ8000DJJQKZ99T0A2Y/2x.webp" alt="Logo" className="w-12 h-12 rounded-lg drop-shadow-lg drop-shadow-black/60" />
         <h1 className="text-2xl font-bold text-shadow-md text-shadow-black">Hvdras Bot</h1>
         <Space />
         <Box className="flex flex-row gap-4 w-80">
@@ -18,16 +18,16 @@ export default function AppLayout({}: Props) {
             className="text-lg rounded-lg"
             active={location.pathname === '/'}
             label="Home"
-            leftSection={<img src="https://cdn.7tv.app/emote/01GQS0ES780009VDYS78D30Z8Z/1x.avif" />}></NavLink>
+            leftSection={<img src="https://cdn.7tv.app/emote/01GQS0ES780009VDYS78D30Z8Z/1x.webp" />}></NavLink>
           <NavLink
             href="/commands"
             className="text-lg rounded-lg"
             active={location.pathname === '/commands'}
             label="Commands"
-            leftSection={<img src="https://cdn.7tv.app/emote/01FJAV4EJG000FZHS49NWQRPND/1x.avif" />}></NavLink>
+            leftSection={<img src="https://cdn.7tv.app/emote/01FJAV4EJG000FZHS49NWQRPND/1x.webp" />}></NavLink>
         </Box>
       </Box>
-      <Box className="">
+      <Box className="flex-grow overflow-hidden">
         {/* <Stack className="flex flex-col gap-0 pt-2 max-w-64 w-1/8 fixed top-20 z-10" style={{ backgroundColor: 'var(--mantine-color-dark-7)', height: '95vh' }}>
           <NavLink href="/" className="text-lg" active={location.pathname === '/'} label="Home" leftSection={<img src="https://cdn.7tv.app/emote/01GQS0ES780009VDYS78D30Z8Z/1x.avif" />}></NavLink>
           <NavLink
@@ -37,13 +37,12 @@ export default function AppLayout({}: Props) {
             label="Commands"
             leftSection={<img src="https://cdn.7tv.app/emote/01FJAV4EJG000FZHS49NWQRPND/1x.avif" />}></NavLink>
         </Stack> */}
-        <ScrollArea style={{ backgroundColor: 'var(--mantine-color-dark-8)', height: '95vh' }} className="flex-grow">
+        <ScrollArea style={{ backgroundColor: 'var(--mantine-color-dark-8)', height: '100%' }}>
           <Container
-            style={
-              {
-                // paddingLeft: '16rem',
-              }
-            }>
+            style={{
+              height: '80vh',
+              // paddingLeft: '16rem',
+            }}>
             <Outlet />
           </Container>
         </ScrollArea>
--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,12 +1,31 @@
-import { Stack } from '@mantine/core'
+import { Anchor, Code, Stack } from '@mantine/core'
 
 type Props = {}
 
 export default function HomePage({}: Props) {
   return (
-    <Stack align="center" justify="center">
-      <div className="text-4xl font-bold text-shadow-md text-shadow-black">Hvdras Bot</div>
-      <div className="text-2xl font-bold text-shadow-md text-shadow-black">a dumb fucking bot</div>
+    <Stack align="center" justify="center" h="100%">
+      <img src="https://cdn.7tv.app/emote/01HNK8DGF0000FG935RNS75APG/4x.webp" alt="Logo" className="w-32 h-32 rounded-lg drop-shadow-lg drop-shadow-black/60" />
+      <div className="text-4xl font-bold text-shadow-md text-shadow-black text-white">Hvdras Bot</div>
+      <div className="text-2xl font-bold text-shadow-md text-shadow-black">a dumbassbitchassidiot bot</div>
+      <div className="text-2xl font-bold text-shadow-md text-shadow-black mt-4">
+        Type{' '}
+        <Code style={{ fontSize: '1.5rem' }} className=" text-gray-500">
+          -join
+        </Code>{' '}
+        in{' '}
+        <Anchor className="text-2xl!" href="https://twitch.tv/hvdras" target="hvdras">
+          hvdras' channel
+        </Anchor>{' '}
+        to get started
+      </div>
+      <div className="text-2xl font-bold text-shadow-md text-shadow-black mt-4">
+        Type{' '}
+        <Code style={{ fontSize: '1.5rem' }} className=" text-gray-500">
+          -part
+        </Code>{' '}
+        for the bot to leave the channel
+      </div>
     </Stack>
   )
 }
